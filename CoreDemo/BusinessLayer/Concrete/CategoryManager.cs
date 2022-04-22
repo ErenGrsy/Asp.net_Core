008d2ba4346d6afb917311e90284106d200f4d43
ErenGrsy <104207680+ErenGrsy@users.noreply.github.com>
2022-04-22 15:57:56 +0300

    ASP.Net_Core Çalışması
    Asp.net Core mimarisinin bir kısmının bitmiş hali üzerinde çalışmaya devam ediyorum...

---
+++ CoreDemo/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository efCategoryRespository;
+
+        public CategoryManager()
+        {
+            efCategoryRespository = new EfCategoryRepository();
+        }
+
+        public void CategoryAdd(Category category)
+        {
+
+            efCategoryRespository.Insert(category);
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            efCategoryRespository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            efCategoryRespository.Update(category);
+        }
+
+        public Category GetbyId(int id)
+        {
+            return efCategoryRespository.GetbyId(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCategoryRespository.GetListAll();
+        }
+    }
+}
